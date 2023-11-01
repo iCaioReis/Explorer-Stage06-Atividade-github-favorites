5852c0bede153fd1d547068ffd73c8fe6d4e0d5f
iCaioReis <caioreis3186@gmail.com>
2023-10-31 23:36:37 -0300

    Add Favorites

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,24 +1,61 @@
+export class GithubUser {
+    static search(username){
+        const endpoint = `https://api.github/users/${username}`
+    }
+}
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+    }
+    load() {
+        const entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    delete(user){
+        const filteredEntries = this.entries.filter(entry => {
+            entry.login !== user.login
+        })
+        this.entries = filteredEntries
+        console.log(this.entries)
+        this.update()
     }
 }
+
+
 
 export class FavoritesView extends Favorites {
     constructor(root) {
         super(root)
 
+        this.tbody = this.root.querySelector('table tbody')
+
         this.update()
     }
-
     update() {
         this.removeAllTr()
 
-        [{
-            login: 'iCaioReis',
-            name: "Caio Reis",
-            public_repos: ''
-        }]
+        this.entries.forEach(user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+            
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+                if(isOk){
+                    this.delete(user)
+                
+                }
+            }
+
+            this.tbody.append(row)
+        })
     }
 
     createRow(){
@@ -26,14 +63,14 @@ export class FavoritesView extends Favorites {
 
         tr.innerHTML = `
         <td class="user">
-            <img src="https://github.com/iCaioReis.png" alt="">
-                <a href="https://github.com/iCaioReis" target="_blank">
-                    <p>Caio Reis</p>
-                    <span>iCaioReis</span>
+            <img src="" alt="">
+                <a href="" target="_blank">
+                    <p></p>
+                    <span></span>
                 </a>
         </td>
-        <td class="repositories">30</td>
-        <td class="followers">3</td>
+        <td class="repositories"></td>
+        <td class="followers"></td>
         <td>
             <button class="remove">&times;</button>
         </td>`
@@ -42,9 +79,7 @@ export class FavoritesView extends Favorites {
     }
 
     removeAllTr() {
-        const tbody = this.root.querySelector('table tbody')
-
-        tbody.querySelectorAll('tr').forEach((tr) => {
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
         })
     }
