1f0390ce3c55902da1965ead9ed838156ac2f94d
iCaioReis <caioreis3186@gmail.com>
2023-11-03 23:28:26 -0300

    Finishing

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,20 +1,5 @@
-export class GithubUser {
-    static search(username) {
-      const endpoint = `https://api.github.com/users/${username}`
-  
-      return fetch(endpoint)
-      .then(data => data.json())
-      .then(({ login, name, public_repos, followers }) => ({
-        login,
-        name,
-        public_repos,
-        followers
-      }))
-    }
-  }
-  
-
-  export class Favorites {
+import { GithubUser } from "../gitHubUser.js"
+export class Favorites {
     constructor(root) {
       this.root = document.querySelector(root)
       this.load()
@@ -23,9 +8,30 @@ export class GithubUser {
     load() {
       this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
     }
+
+    save(){
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
   
     async add(username) {
-      const user = await GithubUser.search(username)
+        try {
+            const userExists = this.entries.find(entry => entry.login === username)
+            if(userExists){
+                throw new Error('Usuário já existe!')
+            }
+
+            const user = await GithubUser.search(username)
+            if(user.login === undefined){
+                throw new Error('Usuário não encontrado! ')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+        } catch (error) {
+            alert(error.message)
+        }
+      
     }
   
     delete(user) {
@@ -34,6 +40,7 @@ export class GithubUser {
   
       this.entries = filteredEntries
       this.update()
+      this.save()
     }
   }
   
@@ -64,6 +71,7 @@ export class GithubUser {
   
         row.querySelector('.user img').src = `https://github.com/${user.login}.png`
         row.querySelector('.user img').alt = `Imagem de ${user.name}`
+        row.querySelector('.user a').href =  `https://github.com/${user.login}`
         row.querySelector('.user p').textContent = user.name
         row.querySelector('.user span').textContent = user.login
         row.querySelector('.repositories').textContent = user.public_repos
